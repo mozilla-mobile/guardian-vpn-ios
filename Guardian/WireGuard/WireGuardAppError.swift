07b92b364e4d2aa14095682af1527edf6724927c
Corby Ziesman <cziesman@connected.io>
2019-10-21 10:26:22 -0400

    Reorganize project files

--- Guardian/WireGuard/WireGuardAppError.swift
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-// Copyright © 2018-2019 WireGuard LLC. All Rights Reserved.
-
-protocol WireGuardAppError: Error {
-    typealias AlertText = (title: String, message: String)
-
-    var alertText: AlertText { get }
-}
