b6af456859c1a71ee9d24bc806339e1233050bfa
Corby Ziesman <cziesman@connected.io>
2019-10-29 10:32:09 -0400

    reorganize files into MVC file structure

--- Guardian/ViewControllers/LoadingViewController.swift
+++ Guardian/ViewControllers/LoadingViewController.swift
@@ -17,14 +17,14 @@ class LoadingViewController: UIViewController {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     init(accountManager: AccountManaging, coordinatorDelegate: Navigating) {
         self.accountManager = accountManager
         self.coordinatorDelegate = coordinatorDelegate
         super.init(nibName: String(describing: LoadingViewController.self), bundle: Bundle.main)
 
         handleNavigationWithDelay(1)
-        
+
         self.accountManager.setupFromAppLaunch { [weak self] result in
             DispatchQueue.main.async {
                 switch result {
@@ -36,7 +36,7 @@ class LoadingViewController: UIViewController {
             }
         }
     }
-    
+
     private func handleNavigationWithDelay(_ delay: Int) {
         Observable.combineLatest(
             Observable.just(()).delay(.seconds(delay), scheduler: MainScheduler.instance),
