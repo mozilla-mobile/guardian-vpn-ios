717a56019c2693e04d9a397ab384c39aaff3777a
Rosalyn Kingsmill <rkingsmill@connected.io>
2019-11-08 10:21:15 -0500

    move commented out code to separate function

--- FirefoxPrivateNetworkVPN/Singletons/NavigationCoordinator.swift
+++ FirefoxPrivateNetworkVPN/Singletons/NavigationCoordinator.swift
@@ -61,9 +61,7 @@ class NavigationCoordinator: NavigationCoordinating {
                 if user.hasTooManyDevices {
                     self?.navigate(from: .home, to: .settings)
                     self?.navigate(from: .settings, to: .devices)
-//                    if let tabBarController = self?.currentViewController as? GuardianTabBarController {
-//                        tabBarController.tabBar.items?[0].isEnabled = false
-//                    }
+                    disableHomeTab()
                 }
 
                 // To Home
@@ -119,5 +117,9 @@ class NavigationCoordinator: NavigationCoordinating {
                 return
             }
         }
+    }
+    
+    func disableHomeTab() {
+//        tabBarController.tabBar.items?[0].isEnabled = false
     }
 }
