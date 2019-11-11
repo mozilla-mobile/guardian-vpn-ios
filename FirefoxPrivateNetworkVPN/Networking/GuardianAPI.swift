155881f95c7c196fe66ba7459f3f5c12ef318268
Rosalyn Kingsmill <rkingsmill@connected.io>
2019-11-11 17:28:20 -0500

    Modify GuardianAPI to use the `unwrap` extension on Result before converting the data Change AddDevice function to use Error instead of GuardianAPIError in it's completion

--- FirefoxPrivateNetworkVPN/Networking/GuardianAPI.swift
+++ FirefoxPrivateNetworkVPN/Networking/GuardianAPI.swift
@@ -15,54 +15,58 @@ class GuardianAPI: NetworkRequesting {
 
     static func initiateUserLogin(completion: @escaping (Result<LoginCheckpointModel, Error>) -> Void) {
         let urlRequest = GuardianURLRequestBuilder.urlRequest(request: .login, type: .POST)
-        NetworkLayer.fireURLRequest(with: urlRequest) { result in
-            completion(result.flatMap { $0.convert(to: LoginCheckpointModel.self) })
+        NetworkLayer.fire(urlRequest: urlRequest) { result in
+            completion(result
+                .unwrap()
+                .flatMap { $0.convert(to: LoginCheckpointModel.self) }
+            )
         }
     }
 
     static func accountInfo(token: String, completion: @escaping (Result<User, Error>) -> Void) {
         let urlRequest = GuardianURLRequestBuilder.urlRequest(request: .account, type: .GET, httpHeaderParams: headers(with: token))
-        NetworkLayer.fireURLRequest(with: urlRequest) { result in
-            completion(result.flatMap { $0.convert(to: User.self) })
+        NetworkLayer.fire(urlRequest: urlRequest) { result in
+            completion(result
+                .unwrap()
+                .flatMap { $0.convert(to: User.self) }
+            )
         }
     }
 
     static func verify(urlString: String, completion: @escaping (Result<VerifyResponse, Error>) -> Void) {
         let urlRequest = GuardianURLRequestBuilder.urlRequest(fullUrlString: urlString, type: .GET)
-        NetworkLayer.fireURLRequest(with: urlRequest) { result in
-            completion(result.flatMap { $0.convert(to: VerifyResponse.self) })
+        NetworkLayer.fire(urlRequest: urlRequest) { result in
+            completion(result
+                .unwrap()
+                .flatMap { $0.convert(to: VerifyResponse.self) }
+            )
         }
     }
 
     static func availableServers(with token: String, completion: @escaping (Result<[VPNCountry], Error>) -> Void) {
         let urlRequest = GuardianURLRequestBuilder.urlRequest(request: .retrieveServers, type: .GET, httpHeaderParams: headers(with: token))
-        NetworkLayer.fireURLRequest(with: urlRequest) { result in
+        NetworkLayer.fire(urlRequest: urlRequest) { result in
             completion(result
+                .unwrap()
                 .flatMap { $0.convert(to: [String: [VPNCountry]].self) }
-                .map { $0["countries"]! })
+                .map { $0["countries"]! }
+            )
         }
     }
 
-    static func addDevice(with token: String, body: [String: Any], completion: @escaping (Result<Device, GuardianAPIError>) -> Void) {
+    static func addDevice(with token: String, body: [String: Any], completion: @escaping (Result<Device, Error>) -> Void) {
         guard let data = try? JSONSerialization.data(withJSONObject: body) else {
-            completion(.failure(.couldNotCreateBody))
+            completion(.failure(GuardianFailReason.couldNotCreateBody))
             return
         }
 
         let urlRequest = GuardianURLRequestBuilder.urlRequest(request: .addDevice, type: .POST, httpHeaderParams: headers(with: token), body: data)
-        NetworkLayer.fire(urlRequest: urlRequest, dataHandler: { result in
-            switch result {
-            case .success(let data):
-                if let device = try? data.convert(to: Device.self).get() {
-                    completion(.success(device))
-                    return
-                }
-            case .failure(let error):
-                print(error)
-                completion(.failure(error))
-//            completion(result.flatMap { try $0.convert(to: Device.self) })
-            }
-        })
+        NetworkLayer.fire(urlRequest: urlRequest) { result in
+            completion(result
+                .unwrap()
+                .flatMap { $0.convert(to: Device.self) }
+            )
+        }
     }
 
     static func removeDevice(with token: String, deviceKey: String, completion: @escaping (Result<Void, Error>) -> Void) {
@@ -73,18 +77,18 @@ class GuardianAPI: NetworkRequesting {
 
         let urlRequest = GuardianURLRequestBuilder.urlRequest(request: .removeDevice(encodedKey), type: .DELETE, httpHeaderParams: headers(with: token))
 
-        NetworkLayer.fire(urlRequest: urlRequest, errorHandler: completion)
+        NetworkLayer.fire(urlRequest: urlRequest) { result in
+            switch result {
+            case .success:
+                completion(.success(()))
+            case.failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 
     private static func headers(with token: String) -> [String: String] {
         return ["Authorization": "Bearer \(token)",
                 "Content-Type": "application/json"]
     }
-}
-
-enum GuardianAPIError: Error {
-    case addDeviceFailure(Data)
-    case couldNotCreateBody
-    case other(Error)
-    case errorWithData(Error, Data?)
 }
