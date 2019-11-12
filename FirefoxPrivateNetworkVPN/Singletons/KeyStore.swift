9922c993dc60ea74f9dead2f7363179abc372a17
Rosalyn Kingsmill <rkingsmill@connected.io>
2019-11-12 17:49:58 -0500

    Make verificationToken not optional and remove userEmail from keyStore Consolidate init functions into two - one that handles the init from appLaunch with the stored credentials and the other that handles the init from the verify call

--- FirefoxPrivateNetworkVPN/Singletons/KeyStore.swift
+++ FirefoxPrivateNetworkVPN/Singletons/KeyStore.swift
@@ -14,65 +14,33 @@ import Foundation
 class Credentials: UserDefaulting {
     static var userDefaultsKey = "credentials"
 
-//    static let shared: Credentials = {
-//        let instance = Credentials()
-//        //
-//        return instance
-//    }()
-
-    private let userEmail: String?
-
     private(set) var deviceKeys: DeviceKeys {
         didSet {
             saveToUserDefaults()
         }
     }
 
-    private(set) var verificationToken: String? {
+    private(set) var verificationToken: String {
         didSet {
             saveToUserDefaults()
         }
     }
 
-    init() {
-        if let userDefaults = Credentials.fetchFromUserDefaults() {
-            deviceKeys = userDefaults.deviceKeys
-            verificationToken = userDefaults.verificationToken
-            userEmail = userDefaults.userEmail
-            return
-        }
-        let privateKey = Curve25519.generatePrivateKey()
-        deviceKeys = DeviceKeys(privateKey: privateKey, publicKey: Curve25519.generatePublicKey(fromPrivateKey: privateKey))
-        userEmail = nil
+    init(with userDefaults: Credentials) {
+        deviceKeys = userDefaults.deviceKeys
+        verificationToken = userDefaults.verificationToken
     }
 
     init(with verification: VerifyResponse) {
-        if let userDefaults = Credentials.fetchFromUserDefaults(),
-            verification.user.email == userDefaults.userEmail {
-            deviceKeys = userDefaults.deviceKeys
-            verificationToken = userDefaults.verificationToken
-            userEmail = userDefaults.userEmail
-            return
-        }
-
         Credentials.removeFromUserDefaults()
         let privateKey = Curve25519.generatePrivateKey()
         deviceKeys = DeviceKeys(privateKey: privateKey, publicKey: Curve25519.generatePublicKey(fromPrivateKey: privateKey))
-        userEmail = verification.user.email
         verificationToken = verification.token
     }
 
-//    func resetDevice(privateKey: Data = Curve25519.generatePrivateKey()) {
-//        deviceKeys = DeviceKeys(privateKey: privateKey, publicKey: Curve25519.generatePublicKey(fromPrivateKey: privateKey))
-//    }
-//
     func setVerification(token: String) {
         verificationToken = token
     }
-//
-//    func clearVerificationToken() {
-//        verificationToken = nil
-//    }
 }
 
 struct DeviceKeys: Codable {
