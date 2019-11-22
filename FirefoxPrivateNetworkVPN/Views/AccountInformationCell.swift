94cac3fde7ee7e0458b46ca0111d5458fc7f82ba
Paul Fangchen Huang <phuang@connected.io>
2019-11-22 08:03:15 -0500

    Added warning icon view to the settings cell

--- FirefoxPrivateNetworkVPN/Views/AccountInformationCell.swift
+++ FirefoxPrivateNetworkVPN/Views/AccountInformationCell.swift
@@ -14,6 +14,7 @@ import UIKit
 class AccountInformationCell: UITableViewCell {
 
     @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var accessoryIconImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
 
     static let height: CGFloat = 55.0
