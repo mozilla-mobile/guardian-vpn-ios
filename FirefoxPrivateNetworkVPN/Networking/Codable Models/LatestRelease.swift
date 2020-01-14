15203e111ab2af20d8690c85ec623a6ad0477877
Rosalyn Kingsmill <rkingsmill@connected.io>
2020-01-14 17:03:26 -0500

    Add lazy var isUpdateRequired that compares the latest version and the minimum version to determine if the update is required

--- FirefoxPrivateNetworkVPN/Networking/Codable Models/LatestRelease.swift
+++ FirefoxPrivateNetworkVPN/Networking/Codable Models/LatestRelease.swift
@@ -13,32 +13,48 @@ import UIKit
 
 struct LatestRelease: UserDefaulting {
     static var userDefaultsKey = "LatestRelease"
-
+    
     let version: String
-    let isRequired: Bool
+    let minimum: String
     let dateRetrieved: Date
-
-    var status: LatestReleaseStatus {
+    
+    lazy var status: LatestReleaseStatus = {
         if version == UIApplication.appVersion {
             return .none
-        } else if isRequired {
+        } else if isUpdateRequired {
             return .required
         } else {
             return .available
         }
-    }
-
+    }()
+    
+    lazy var isUpdateRequired: Bool = {
+        let first = version.components(separatedBy: ".")
+        let second = minimum.components(separatedBy: ".")
+        let maxIndex = first >= second.endIndex ? first.endIndex : second.endIndex
+        
+        for index in 0..<maxIndex {
+            let firstElement = first.indices.contains(index) ? first[index] : "0"
+            let secondElement = second.indices.contains(index) ? second[index] : "0"
+            let comparisonResult = firstElement.compare(secondElement, options: .numeric)
+            guard comparisonResult == .orderedSame else {
+                return comparisonResult == .orderedAscending
+            }
+        }
+        return false
+    }()
+    
     init(with release: Release) {
-        version = release.version
-        isRequired = release.required
+        version = release.latestVersion
+        minimum = release.minimumVersion
         dateRetrieved = release.dateRetrieved
     }
-
+    
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-
+        
         version = try container.decode(String.self, forKey: .version)
-        isRequired = try container.decode(Bool.self, forKey: .isRequired)
+        minimum = try container.decode(String.self, forKey: .minimum)
         dateRetrieved = try container.decode(Date.self, forKey: .dateRetrieved)
     }
 }
