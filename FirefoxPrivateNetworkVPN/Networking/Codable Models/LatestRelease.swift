7a9fe3dc4464e639996eca07851920dd1c64d7f2
Rosalyn Kingsmill <rkingsmill@connected.io>
2020-01-14 17:12:35 -0500

    Change lazy vars to computed properties

--- FirefoxPrivateNetworkVPN/Networking/Codable Models/LatestRelease.swift
+++ FirefoxPrivateNetworkVPN/Networking/Codable Models/LatestRelease.swift
@@ -13,12 +13,12 @@ import UIKit
 
 struct LatestRelease: UserDefaulting {
     static var userDefaultsKey = "LatestRelease"
-    
+
     let version: String
     let minimum: String
     let dateRetrieved: Date
-    
-    lazy var status: LatestReleaseStatus = {
+
+    var status: LatestReleaseStatus {
         if version == UIApplication.appVersion {
             return .none
         } else if isUpdateRequired {
@@ -26,13 +26,13 @@ struct LatestRelease: UserDefaulting {
         } else {
             return .available
         }
-    }()
-    
-    lazy var isUpdateRequired: Bool = {
+    }
+
+    var isUpdateRequired: Bool {
         let first = version.components(separatedBy: ".")
         let second = minimum.components(separatedBy: ".")
-        let maxIndex = first >= second.endIndex ? first.endIndex : second.endIndex
-        
+        let maxIndex = first.endIndex >= second.endIndex ? first.endIndex : second.endIndex
+
         for index in 0..<maxIndex {
             let firstElement = first.indices.contains(index) ? first[index] : "0"
             let secondElement = second.indices.contains(index) ? second[index] : "0"
@@ -42,17 +42,17 @@ struct LatestRelease: UserDefaulting {
             }
         }
         return false
-    }()
-    
+    }
+
     init(with release: Release) {
         version = release.latestVersion
         minimum = release.minimumVersion
         dateRetrieved = release.dateRetrieved
     }
-    
+
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        
+
         version = try container.decode(String.self, forKey: .version)
         minimum = try container.decode(String.self, forKey: .minimum)
         dateRetrieved = try container.decode(Date.self, forKey: .dateRetrieved)
