8f322e4886a155259e13df9874a98661deb010ce
Rosalyn Kingsmill <rkingsmill@connected.io>
2020-01-17 13:53:28 -0500

    Fix bug where it was checking latest against minimum instead of current agains minimum

--- FirefoxPrivateNetworkVPN/Networking/Codable Models/ReleaseInfo.swift
+++ FirefoxPrivateNetworkVPN/Networking/Codable Models/ReleaseInfo.swift
@@ -14,47 +14,51 @@ import UIKit
 struct ReleaseInfo: UserDefaulting {
     static var userDefaultsKey = "LatestRelease"
 
-    let version: String
-    let minimum: String
+    private let latestVersion: String
+    private let minimumVersion: String
     let dateRetrieved: Date
 
+    private var currentVersion: String {
+        return UIApplication.appVersion ?? ""
+    }
+
     var status: LatestReleaseStatus {
-        if version == UIApplication.appVersion {
+        if currentVersion >= latestVersion {
             return .none
-        } else if compareLatestToMinimum() == .orderedAscending {
+        } else if compareCurrentToMinimum() == .orderedAscending {
             return .recommended
         } else {
             return .available
         }
     }
 
-    private func compareLatestToMinimum() -> ComparisonResult {
-        let latestVersionArray = version.components(separatedBy: ".")
-        let minimumVersionArray = minimum.components(separatedBy: ".")
-        let maxIndex = latestVersionArray.endIndex >= minimumVersionArray.endIndex ? latestVersionArray.endIndex : minimumVersionArray.endIndex
-
-        for index in 0..<maxIndex {
-            let latestVersionElement = latestVersionArray.indices.contains(index) ? latestVersionArray[index] : "0"
-            let minimumVersionElement = minimumVersionArray.indices.contains(index) ? minimumVersionArray[index] : "0"
-            let comparisonResult = latestVersionElement.compare(minimumVersionElement, options: .numeric)
-            guard comparisonResult == .orderedSame else {
-                return comparisonResult
-            }
-        }
-        return .orderedSame
-    }
-
     init(with release: Release) {
-        version = release.latestVersion
-        minimum = release.minimumVersion
+        latestVersion = release.latestVersion
+        minimumVersion = release.minimumVersion
         dateRetrieved = release.dateRetrieved
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
-        version = try container.decode(String.self, forKey: .version)
-        minimum = try container.decode(String.self, forKey: .minimum)
+        latestVersion = try container.decode(String.self, forKey: .latestVersion)
+        minimumVersion = try container.decode(String.self, forKey: .minimumVersion)
         dateRetrieved = try container.decode(Date.self, forKey: .dateRetrieved)
+    }
+
+    private func compareCurrentToMinimum() -> ComparisonResult {
+        let currentVersionArray = currentVersion.components(separatedBy: ".")
+        let minimumVersionArray = minimumVersion.components(separatedBy: ".")
+        let maxIndex = currentVersionArray.endIndex >= minimumVersionArray.endIndex ? currentVersionArray.endIndex : minimumVersionArray.endIndex
+
+        for index in 0..<maxIndex {
+            let currentVersionElement = currentVersionArray.indices.contains(index) ? currentVersionArray[index] : "0"
+            let minimumVersionElement = minimumVersionArray.indices.contains(index) ? minimumVersionArray[index] : "0"
+            let comparisonResult = currentVersionElement.compare(minimumVersionElement, options: .numeric)
+            guard comparisonResult == .orderedSame else {
+                return comparisonResult
+            }
+        }
+        return .orderedSame
     }
 }
