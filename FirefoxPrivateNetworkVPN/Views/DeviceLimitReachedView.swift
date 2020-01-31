ab924036a843216251b012eb978378cfc0a2dd45
Rosalyn Kingsmill <rkingsmill@connected.io>
2020-01-31 11:51:13 -0500

    Add iPad constraints for device limit reached view

--- FirefoxPrivateNetworkVPN/Views/DeviceLimitReachedView.swift
+++ FirefoxPrivateNetworkVPN/Views/DeviceLimitReachedView.swift
@@ -17,7 +17,7 @@ class DeviceLimitReachedView: UITableViewHeaderFooterView {
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var subtitle: UILabel!
 
-    static let height: CGFloat = UIScreen.isiPad ? 350 : 252.0
+    static let height: CGFloat = UIScreen.isiPad ? 316.0 : 274.0
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,7 +26,11 @@ class DeviceLimitReachedView: UITableViewHeaderFooterView {
 
     private func styleViews() {
         title.text = LocalizedString.devicesLimitTitle.value
+        title.font = UIFont.custom(.metropolis, size: 22)
+        title.textColor = UIColor.custom(.grey50)
         subtitle.text = LocalizedString.devicesLimitSubtitle.value
+        subtitle.font = UIFont.custom(.inter)
+        subtitle.textColor = UIColor.custom(.grey40)
         let customBackgroundView = UIView()
         customBackgroundView.backgroundColor = UIColor.custom(.grey5)
         backgroundView = customBackgroundView
