36b5d80913d3f82bfd2037e397a54811962df3d0
Conjuror <kuang.con@gmail.com>
2020-04-29 17:13:08 +0800

    Snapshot taker initialization

---
+++ FirefoxPrivateNetworkVPNSnapshotsTests/FirefoxPrivateNetworkVPNSnapshotsTests.swift
@@ -0,0 +1,54 @@
+//
+//  FirefoxPrivateNetworkVPNSnapshotsTests
+//  FirefoxPrivateNetworkVPNSnapshotsTests
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+//  Copyright © 2020 Mozilla Corporation.
+//
+
+import XCTest
+
+class FirefoxPrivateNetworkVPNSnapshotsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    override func setUp() {
+        // fastlane snapshot init
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+//        let app = XCUIApplication()
+//        app.launch()
+
+        snapshot("00Launched")
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
